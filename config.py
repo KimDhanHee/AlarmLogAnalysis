b5dda7ab649db8274d569eab02fc4bcbd659282c
kimdanhee <sgoiboy@gmail.com>
2021-07-25 13:50:45 +0900

    설정 config.py 추가

---
+++ config.py
@@ -0,0 +1,11 @@
+START_EVENT = 'alarm_receiver_fire_alarm'
+END_EVENT = 'alarm_dismissed'
+ALARM_SCHEDULE_EVENT = 'alarm_promise_scheduled'
+FILTERED_EVENTS = [
+    'drawable',
+    'ad_request',
+    'ad_loaded',
+    'ad_load_failed',
+    'format',
+    'xday_noti_canceled'
+]
